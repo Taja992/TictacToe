8be327d486501ceaf5751825572f91c38be063b8
Taja <Tharaxui@gmail.com>
2023-10-06 11:08:43 +0200

    Added Gameboard

--- TicTacToe/TicTacToe/src/tictactoe/bll/GameBoard.java
+++ TicTacToe/TicTacToe/src/tictactoe/bll/GameBoard.java
@@ -14,50 +14,31 @@ import tictactoe.gui.controller.TicTacViewController;
 public class GameBoard implements IGameModel
 {
 
-    /**
-     * Returns 0 for player 0, 1 for player 1.
-     *
-     * @return int Id of the next player.
-     */
-    private int currentPlayer = 0;
-    private int f = 0;
 
+    private int currentPlayer = 0;
+    private int [][] board; // Represents the gameboard
+
+    public GameBoard()
+    {
+        board = new int[3][3]; // makes a 3x3 grid and sets every cell to -1 for empty cells
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                board[i][j] = -1;
+            }
+        }
+    }
     public int getNextPlayer()          //TODO Implement this method
     {
-        if (f == 2){
-            if (currentPlayer == 0) {
-                currentPlayer = 1;
-            } else {
-                currentPlayer = 0;
-            }
-            f = 0;
-        }
-        f++;
-        //currentPlayer++;
         return currentPlayer;
-       /* if (currentPlayer == 0) {
-            currentPlayer = 1;
-        } else {
-            currentPlayer = 0;
-        }
-        return currentPlayer;*/
-
-        /*  switch(currentPlayer){
-            case 1:
-                currentPlayer = 0;
-                break;
-            case 0:
-                currentPlayer = 1;
-                break;
-        }
-        //currentPlayer = (currentPlayer == 0) ? 1 : 0;
-        return currentPlayer;*/
     }
 
 
     /**
      * Attempts to let the current player play at the given coordinates. It the
-     * attempt is succesfull the current player has ended his turn and it is the
+     * attempt is successful the current player has ended his turn and it is the
      * next players turn.
      *
      * @param col column to place a marker in.
@@ -65,10 +46,15 @@ public class GameBoard implements IGameModel
      * @return true if the move is accepted, otherwise false. If gameOver == true
      * this method will always return false.
      */
-    public boolean play(int col, int row)
+    public boolean play(int col, int row)        //TODO Implement this method
     {
-        //TODO Implement this method
-        return true;
+        if (board[col][row] == -1) // Check if the cell is empty
+        {
+            board[col][row] = currentPlayer; // Checks which player is playing and sets the square to 0,0 or 1,1 making it unusable
+            currentPlayer = (currentPlayer == 0) ? 1 : 0; // Short form if-else statement I found :D but this changes the players turn
+            return true; //
+        }
+        return false; // if the cell has a number in it, will not let the game progress
     }
 
     public boolean isGameOver()
